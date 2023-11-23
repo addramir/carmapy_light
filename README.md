f4fd962b4bd45d28d35d1fb184db7226447c760b
Yakov Tsepilov <yt4@sanger.ac.uk>
2023-11-23 09:41:35 +0000

    Update tests

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # carmapy_light
-Repo for the python code of the outlers detection algorithm of CARMA
+
+This repository contains the Python code for the outlier detection and PIPs calualtion algorithm of CARMA, originally implemented in R ([CARMA on GitHub](https://github.com/ZikunY/CARMA)).
+
+The full repository for the reimplementation of CARMA in Python can be found [here](https://github.com/hlnicholls/carmapy/tree/0.1.0).
+
+This is a simplified version of CARMA with the following features:
+
+1. It uses only Spike-slab effect size priors and Poisson model priors.
+2. C++ is re-implemented in Python.
+3. The way of storing the configuration list is changed. It uses a string with the list of indexes for causal SNPs instead of a sparse matrix.
+4. Fixed bugs in PIP calculation.
+5. No credible models.
+6. No credible sets, only PIPs.
+7. No functional annotations.
+8. Removed unnecessary parameters.
+
+
+**Use:**
+
+No installation is required; it only has Python code (no C++ code).
+
+See tests for examples.
+
+**Notes:**
+
+If you want to use it for outlier detection, you can specify the number of iterations to 1 (`all_iter=1`); it will be enough.
+
+There are issues when using an LD matrix with a determinant equal to 0. This problem is inherited from the original version and requires investigation.
--- tests/01_R_run.R
+++ tests/01_R_run.R
@@ -10,7 +10,8 @@ suff="test1"
 z_full=fread("01_test_z.csv",header=T,sep=",",data.table=F)
 ld=fread("01_test_ld.csv",header=F,sep=",",data.table=F)
 z=z_full[,"z"]
-R3=CARMA(z.list = list(z),ld.list = list(ld),lambda.list = list(1),output.labels = NULL)
+R3=CARMA(z.list = list(z),ld.list = list(ld),lambda.list = list(1),
+         output.labels = NULL,all.iter = 1)
 R3=R3[[1]]
 pip=cbind(PIP=R3$PIPs)
 
@@ -23,7 +24,8 @@ suff="test2"
 z_full=fread("02_test_z.csv",header=T,sep=",",data.table=F)
 ld=npyLoad("02_test_ld.npy")
 z=z_full[,"z"]
-R3=CARMA(z.list = list(z),ld.list = list(ld),lambda.list = list(1),output.labels = NULL)
+R3=CARMA(z.list = list(z),ld.list = list(ld),lambda.list = list(1),
+         output.labels = NULL,all.iter = 1)
 R3=R3[[1]]
 pip=cbind(PIP=R3$PIPs)
 
@@ -36,9 +38,11 @@ suff="test3"
 z_full=fread("03_test_z.csv",header=T,sep=",",data.table=F)
 ld=fread("03_test_ld.csv",header=F,sep=",",data.table=F)
 z=z_full[,"z"]
-R3=CARMA(z.list = list(z),ld.list = list(ld),lambda.list = list(1),output.labels = NULL)
+R3=CARMA(z.list = list(z),ld.list = list(ld),lambda.list = list(1),
+         output.labels = NULL,all.iter = 1)
 R3=R3[[1]]
 pip=cbind(PIP=R3$PIPs)
 
 fwrite(paste0(suff,"_o.csv"),x=R3$Outliers,col.names = T,row.names=F)
 fwrite(paste0(suff,"_pip.csv"),x=pip,col.names = T,row.names=F)
+
